15fcded0ae7959f24236f659436073be6e51a91c
admodev <admodevcodes@outlook.com>
2022-05-20 11:47:25 -0300

    add rocket.rs get functions test static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Welcome to RustGists!");
+#[macro_use] extern crate rocket;
+
+use std::path::{Path, PathBuf};
+use rocket::fs::NamedFile;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, World!"
+}
+
+#[get("/<file..>")]
+async fn hello(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/hello", routes![hello])
 }
